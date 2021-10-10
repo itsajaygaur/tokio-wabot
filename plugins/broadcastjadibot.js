95e6f43b3e8d3b06fc110aabd288d223d2bb0b6a
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-10 22:24:03 +0530

    Update broadcastjadibot.js

--- plugins/broadcastjadibot.js
+++ plugins/broadcastjadibot.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, usedPrefix, text }) => {
   let teks = text ? text : cc.text
   let content = conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 All Jadibot Broadcast 」')
   for (let id of users) conn.copyNForward(id, content, true)
-  conn.reply(m.chat, `_Berhasil mengirim broadcast ke ${users.length} nomor yang jadi bot_
+  conn.reply(m.chat, `_Send a broadcast message to ${users.length} number that became bot_
 ${users.map(v => 'wa.me/' + v.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu`).join('\n')}`.trim(), m)
 }
 handler.help = ['broadcastjadibot', 'bcbot'].map(v => v + ' <teks>')
