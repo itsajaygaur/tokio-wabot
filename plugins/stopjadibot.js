8c518ec48ca31cbdf0ffd6f4a9868987afe35774
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-10 23:36:15 +0530

    Update stopjadibot.js

--- plugins/stopjadibot.js
+++ plugins/stopjadibot.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-  if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'Kenapa nggk langsung ke terminalnya?', m)
+  if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'Why dont you go directly to the terminal?', m)
   else {
     await conn.reply(m.chat, 'Goodbye bot :\')', m)
     conn.close()
