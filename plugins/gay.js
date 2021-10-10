6998e0a61e1f0cabe644984787e7488c6840b91c
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-10 23:40:11 +0530

    Update gay.js

--- plugins/gay.js
+++ plugins/gay.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/gay', {
     avatar: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'gay.png', 'siapa disini yang cita citanya menjadi gay dan memperkosa gw gays?', m)
+  }), 'gay.png', 'who here wants to be gay and do fun with me gays?', m)
 }
 
 handler.help = ['gay']
