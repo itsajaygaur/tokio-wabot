a7632094fabb203443ed4aa6409934df901583be
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-10 23:20:45 +0530

    Update ppcouple.js

--- plugins/ppcouple.js
+++ plugins/ppcouple.js
@@ -5,8 +5,8 @@ let handler = async (m, { conn }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.status) throw json
-  await conn.sendFile(m.chat, json.result.male, '', 'cowo', m)
-  await conn.sendFile(m.chat, json.result.female, '', 'cewe', m)
+  await conn.sendFile(m.chat, json.result.male, '', 'boy', m)
+  await conn.sendFile(m.chat, json.result.female, '', 'girl', m)
 }
 handler.help = ['ppcouple', 'ppcp']
 handler.tags = ['internet']
