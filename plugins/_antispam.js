04754c09c90074c4ab44a25a251f0ce7f88baabb
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-10 20:56:37 +0530

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -8,7 +8,7 @@ handler.all = async function (m) {
         if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
             if (this.spam[m.sender].count > 10) {
                 //global.db.data.users[m.sender].banned = true
-                m.reply('*Jangan Spam!!*')
+                m.reply('*Do not Spam!!*')
             }
             this.spam[m.sender].count = 0
             this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
