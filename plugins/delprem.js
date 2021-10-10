cbe434682df2d851bda88f41e76186962f3c0689
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-10 23:42:59 +0530

    Update delprem.js

--- plugins/delprem.js
+++ plugins/delprem.js
@@ -3,11 +3,11 @@ let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
     else who = m.chat
-    if (!who) throw `tag orangnya!`
-    if (!global.prems.includes(who.split`@`[0])) throw 'dia ngga premium!'
+    if (!who) throw `tag the person!`
+    if (!global.prems.includes(who.split`@`[0])) throw 'he is not premium!'
     let index = global.prems.findIndex(v => (v.replace(/[^0-9]/g, '') + '@s.whatsapp.net') === (who.replace(/[^0-9]/g, '') + '@s.whatsapp.net'))
     global.prems.splice(index, 1)
-    conn.reply(m.chat, `@${who.split('@')[0]} sekarang bukan premium!`, m, {
+    conn.reply(m.chat, `@${who.split('@')[0]} now not premium!`, m, {
         contextInfo: {
             mentionedJid: [who]
         }
