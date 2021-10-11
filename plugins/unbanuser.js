45043b9742936d7e3bcd068f917917188e8c95ad
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-11 10:28:39 +0530

    Update unbanuser.js

--- plugins/unbanuser.js
+++ plugins/unbanuser.js
@@ -1,13 +1,13 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, text}) => {
-    if (!text) throw 'Siapa yang mau di unbanned?'
+    if (!text) throw 'Who wants to be unbanned?'
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw 'Tag salah satu lah'
+    if (!who) throw 'Tag one'
     let users = global.db.data.users
     users[who].banned = false
-    conn.reply(m.chat, `berhasil unbanned`, m)
+    conn.reply(m.chat, `successfully unbanned`, m)
 }
 handler.help = ['ban']
 handler.tags = ['owner']
