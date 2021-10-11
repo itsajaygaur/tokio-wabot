f60714f40b94cd67201857a177994c2617b35956
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-11 21:31:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 global.owner = ['37122222642'] // Put your number here
-global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.mods = ['37122222642'] // Want some help?
+global.prems = ['37122222642'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
