cdb3b4f2774de61ddb1a067d14f8d7ce9a3c562e
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-12 22:59:22 +0530

    Update setdesk_gc.js

--- plugins/setdesk_gc.js
+++ plugins/setdesk_gc.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, args }) => {
  await conn.groupUpdateDescription(m.chat, `${args.join(" ")}`);
-  m.reply('Sukses mengganti deskripsi group')
+  m.reply('Successfully changed the group description')
 }
 
 handler.help = ['Setdesk <text>']
