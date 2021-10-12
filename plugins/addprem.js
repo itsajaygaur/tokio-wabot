18c95157bec5fc04187d15dded98cc269622ec57
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-12 15:37:15 +0530

    Update addprem.js

--- plugins/addprem.js
+++ plugins/addprem.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
     else who = m.chat
-    if (!who) throw `tag orangnya!`
+    if (!who) throw `tag the person!`
     if (global.prems.includes(who.split`@`[0])) throw 'he is premium!'
     global.prems.push(`${who.split`@`[0]}`)
     conn.reply(m.chat, `@${who.split`@`[0]} now premium!`, m, {
@@ -15,7 +15,7 @@ let handler = async (m, { conn, text }) => {
 }
 handler.help = ['addprem [@user]']
 handler.tags = ['owner']
-handler.command = /^(add|tambah|\+)prem$/i
+handler.command = /^(add|plus|\+)prem$/i
 
 handler.rowner = true
 
