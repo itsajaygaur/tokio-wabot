a5290eff705b46cf655e96f6325e998bc3c7c48f
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-12 22:57:10 +0530

    Update setbotname.js

--- plugins/setbotname.js
+++ plugins/setbotname.js
@@ -1,9 +1,9 @@
 // NurNurz
 let handler = async (m, { conn, text }) => {
-   if (!text) throw `Masukan Nama Baru Untuk Bot`
+   if (!text) throw `Enter New Name For Bot`
      try {
         await conn.updateProfileName(text)
-        conn.reply(m.chat, 'Sukses Mengganti Nama Bot', m)
+        conn.reply(m.chat, 'Success in Renaming Bot', m)
      } catch (e) {
        console.log(e)
        throw `Error`
