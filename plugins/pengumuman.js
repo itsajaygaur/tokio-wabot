432337deed6bb65d80dfde8277136bfeef9b8c65
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-12 12:22:30 +0530

    Update pengumuman.js

--- plugins/pengumuman.js
+++ plugins/pengumuman.js
@@ -22,9 +22,9 @@ let handler = async (m, { conn, text, participants }) => {
   )
   await conn.relayWAMessage(msg)
 }
-handler.help = ['pengumuman', 'announce', 'hidetag'].map(v => v + ' [teks]')
+handler.help = ['announcement', 'announce', 'hidetag'].map(v => v + ' [teks]')
 handler.tags = ['group']
-handler.command = /^(pengumuman|announce|hiddentag|hidetag)$/i
+handler.command = /^(announcement|announce|hiddentag|hidetag)$/i
 
 handler.group = true
 handler.admin = true
