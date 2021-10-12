e91d85b9a808153d87fdfe2b04447c4f0d0cd20a
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-12 22:39:20 +0530

    Update leavegc.js

--- plugins/leavegc.js
+++ plugins/leavegc.js
@@ -2,20 +2,20 @@ let handler = async (m, { conn, args, command }) => {
     let chat = conn.chats.all().filter(v => v.jid.endsWith('g.us') && !v.read_only)
     if (command.endsWith('all') || command.endsWith('semua')) {
         for (let i = 0; i < chat.length; i++) { // For loops
-            await m.reply('Byee👋, Bot akan keluar dari group', chat[i].jid)
+            await m.reply('Byee👋, The bot will leave the group', chat[i].jid)
             await conn.groupLeave(chat[i].jid)
-            await delay(i * 2000) // Delaynya
+            await delay(i * 2000) // The delay
         }
-        await m.reply('Succes!!')
+        await m.reply('Success!')
     } else if (args[0] || args.length > 0) {
-        let ada = chat.find(bot => bot.jid == args[0]) // Apakah botnya ada disitu
-        if (!ada) throw 'Bot tidak berada dalam group itu!!'
-        await m.reply('Byee👋, Bot akan keluar dari group', args[0])
+        let ada = chat.find(bot => bot.jid == args[0]) // Is the bot there
+        if (!ada) throw 'The bot is not in that group!'
+        await m.reply('Byee👋, The bot will leave the group', args[0])
         await conn.groupLeave(args[0])
-        await m.reply('Succes!!')
+        await m.reply('Success!')
     } else {
         if (!m.isGroup) return global.dfail('group', m, conn)
-        await m.reply('Byee👋, Bot akan keluar dari group', m.chat) // WKWKW pesannya sama semua. gk kreatif :v
+        await m.reply('Byee👋, The bot will leave the group', m.chat) // WKWKW all the messages are the same. not creative :v
         await conn.groupLeave(m.chat)
     }
 }
