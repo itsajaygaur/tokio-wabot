b8b6cd91acc5067027c64dc7691870db9192cd56
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-13 15:46:08 +0530

    Update pinterest.js

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `Contoh: ${usedPrefix + command} minecraft`
+  if (!text) throw `Example: ${usedPrefix + command} minecraft`
   let res = await fetch(global.API('zeks', '/api/pinimg', {
     q: encodeURI(text)
   }, 'apikey'))
