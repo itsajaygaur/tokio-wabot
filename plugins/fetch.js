f0f76da343ef3937788d072ee43ee63748962e51
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-13 14:45:04 +0530

    Update fetch.js

--- plugins/fetch.js
+++ plugins/fetch.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let util = require('util')
 let handler = async (m, { text }) => {
-  if (!/^https?:\/\//.test(text)) throw 'Awali *URL* dengan http:// atau https://'
+  if (!/^https?:\/\//.test(text)) throw 'Prefix *URL* with http:// or https://'
   let _url = new URL(text)
   let url = global.API(_url.origin, _url.pathname, Object.fromEntries(_url.searchParams.entries()), 'APIKEY')
   let res = await fetch(url)
@@ -19,7 +19,7 @@ let handler = async (m, { text }) => {
     m.reply(txt.slice(0, 65536) + '')
   }
 }
-handler.help = ['fetch', 'get'].map(v => v + ' <url>')
+handler.help = ['fetch/get'].map(v => v + ' <url>')
 handler.tags = ['internet']
 handler.command = /^(fetch|get)$/i
 
