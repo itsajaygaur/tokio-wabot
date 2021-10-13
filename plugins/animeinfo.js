db63f01477135ba30b5fbf29641ab819e1b1eaa0
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-13 13:48:31 +0530

    Update animeinfo.js

--- plugins/animeinfo.js
+++ plugins/animeinfo.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let cheerio = require('cheerio')
 let handler = async(m, { conn, text }) => {
-  if (!text) throw `Masukkan query!`
+  if (!text) throw `Enter query!`
   let res = await fetch(global.API('https://api.jikan.moe', '/v3/search/anime', { q: text }))
   if (!res.ok) throw await res.text()
   let json = await res.json()
@@ -10,19 +10,19 @@ let handler = async(m, { conn, text }) => {
   var genAnim = []
   await fetch(`https://myanimelist.net/anime/${mal_id}`, { method: 'get' }).then(res => res.text()).then(res => { const $ = cheerio.load(res);$('div[class="spaceit_pad"]').each((a, b) => { $(b).each(function(c, d) { $(d).find("a").each(function(e, f) { if ($(f).attr("href").startsWith('/anime/genre/')) { genAnim.push($(f).text()) } }) }) }) })
 let animeingfo = `✨️ *Title:* ${title}
-🎆️ *Episodes:* ${episodes}
+📺 *Episodes:* ${episodes}
 🎗️ *Genre:* ${genAnim.join(", ")}
 ➡️ *Start date:* ${start_date}
-🔚 *End date:* ${end_date}
-💬 *Show Type:* ${type}
-💌️ *Rating:* ${rated}
+⬅️ *End date:* ${end_date}
+🏮 *Show Type:* ${type}
+🔖 *Rating:* ${rated}
 ❤️ *Score:* ${score}
 👥 *Members:* ${members}
-💚️ *Synopsis:* ${synopsis}
+🎴 *Synopsis:* ${synopsis}
 🌐️ *URL*: ${url}`
   conn.sendFile(m.chat, image_url, '', animeingfo, m)
 }
-handler.help = ['anime <judul>']
+handler.help = ['anime <title>']
 handler.tags = ['internet']
 handler.command = /^(anime|animeinfo)$/i
 //maapin fatur :<
