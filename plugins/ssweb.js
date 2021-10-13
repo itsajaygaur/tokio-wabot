68fda5870edb3757ddca0bfdeff461038ba689a7
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-13 16:03:13 +0530

    Update ssweb.js

--- plugins/ssweb.js
+++ plugins/ssweb.js
@@ -1,12 +1,12 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command, args }) => {
   let full = /f$/i.test(command)
-  if (!args[0]) return conn.reply(m.chat, 'Tidak ada url', m)
+  if (!args[0]) return conn.reply(m.chat, 'No url', m)
   let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
   let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
   conn.sendFile(m.chat, ss, 'screenshot.png', url, m)
 }
-handler.help = ['ss', 'ssf'].map(v => v + ' <url>')
+handler.help = ['ss/ssf'].map(v => v + ' <url>')
 handler.tags = ['internet']
 handler.command = /^ss(web)?f?$/i
 handler.owner = false
