d83b7b3d7de142eb5bfd0a5ed240c400e8f1c302
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-13 22:21:14 +0530

    Update lockCmdWithMedia.js

--- plugins/lockCmdWithMedia.js
+++ plugins/lockCmdWithMedia.js
@@ -1,5 +1,5 @@
 module.exports = Object.assign(async function handler(m, { command }) {
-    if (!m.quoted) throw 'Reply Pesan!'
+    if (!m.quoted) throw 'Reply Message!'
     if (!m.quoted.fileSha256) throw 'SHA256 Hash Missing'
     let sticker = global.db.data.sticker
     let hash = m.quoted.fileSha256.toString('hex')
