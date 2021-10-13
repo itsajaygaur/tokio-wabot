f9c337123d5166e46834889d1534a1969c2fcb7d
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-13 16:09:52 +0530

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'Istri kartun', m)
+  conn.sendFile(m.chat, json.url, '', 'UωU', m)
 }
 handler.help = ['waifu']
 handler.tags = ['internet']
