70a4cf68109e0293b47729facbc6d4a4dea2cc2b
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-13 21:59:49 +0530

    Update unregister.js

--- plugins/unregister.js
+++ plugins/unregister.js
@@ -7,7 +7,7 @@ let handler = async function (m, { args }) {
   user.registered = false
   m.reply(`Unreg succeeded!`)
 }
-handler.help = ['', 'ister'].map(v => 'unreg' + v + ' <SN|SERIAL NUMBER>')
+handler.help = ['/unregister'].map(v => 'unreg' + v + ' <SN|SERIAL NUMBER>')
 handler.tags = ['exp']
 
 handler.command = /^unreg(ister)?$/i
