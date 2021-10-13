3ff6f208fb2117b81dd83cad0412334eb5410670
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-13 14:35:19 +0530

    Update charainfo.js

--- plugins/charainfo.js
+++ plugins/charainfo.js
@@ -1,18 +1,18 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
-  if (!text) throw `Masukkan query!`
+  if (!text) throw `Enter the query!`
   let res = await fetch(global.API('https://api.jikan.moe', '/v3/search/character', { q: text }))
   if (!res.ok) throw await res.text()
   let json = await res.json()
   let { name, alternative_names, url, image_url, type } = json.results[0]
-let charaingfo = `💬 *Name:* ${name}
-💭 *Nickname:* ${alternative_names}
+let charaingfo = `🧧 *Name:* ${name}
+🏮 *Nickname:* ${alternative_names}
 🔗 *Link*: ${url}
-👤 *Character Type*: ${type}`
+🧩 *Character Type*: ${type}`
 
   conn.sendFile(m.chat, image_url, '', charaingfo, m)
 }
-handler.help = ['character <nama>']
+handler.help = ['character <name>']
 handler.tags = ['internet']
 handler.command = /^(chara|character)$/i
 //kyaa jangan biarkan wabot-aq terbengkalai sampai nurutomo kembali
