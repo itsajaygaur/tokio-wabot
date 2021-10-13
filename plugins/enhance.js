ff95f5c9d515e1ff2b2c3bac80007451df855ff4
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-13 20:29:33 +0530

    Update enhance.js

--- plugins/enhance.js
+++ plugins/enhance.js
@@ -4,8 +4,8 @@ const FormData = require('form-data')
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'Tidak ada foto'
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
+  if (!mime) throw 'No picture'
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} not supported`
   let img = await q.download()
   let body = new FormData
   body.append('image', img, 'image')
@@ -14,9 +14,9 @@ let handler = async (m) => {
     body
   })
   if (res.status !== 200) throw await res.json()
-  await conn.sendFile(m.chat, await res.buffer(), 'hd.jpg', 'Nih, hd kan?', m)
+  await conn.sendFile(m.chat, await res.buffer(), 'hd.jpg', 'Here, HD right?', m)
 }
-handler.help = ['hd (caption|reply media)', 'enhance (caption|reply media)']
+handler.help = ['hd/enhance (caption|reply media)']
 handler.tags = ['tools']
 handler.command = /^(hd|enhance)$/i
 
