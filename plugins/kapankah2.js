03584752fc49ea2091c66992e1977c5bdc1e1018
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-13 13:34:17 +0530

    Update kapankah2.js

--- plugins/kapankah2.js
+++ plugins/kapankah2.js
@@ -1,16 +1,16 @@
 let handler = async (m, { conn, command, text }) => {
   conn.reply(m.chat, `
-*Question:* ${command} ${text}
-*Answer:* ${Math.floor(Math.random() * 10)} ${pickRandom(['second', 'minute', 'o\'clock', 'day', 'week', 'month', 'year', 'decade', 'century'])} again ...
+*Pertanyaan:* ${command} ${text}
+*Jawaban:* ${Math.floor(Math.random() * 10)} ${pickRandom(['detik', 'menit', 'jam', 'hari', 'minggu', 'bulan', 'tahun', 'dekade', 'abad'])} lagi ...
 `.trim(), m, m.mentionedJid ? {
   contextInfo: {
     mentionedJid: m.mentionedJid
   }
 } : {})
 }
-handler.help = ['', 'y'].map(v => 'when' + v + ' <question>')
-handler.tags = ['shell']
-handler.command = /^when(y)?$/i
+handler.help = ['', 'kah'].map(v => 'kapan' + v + ' <pertanyaan>')
+handler.tags = ['Only for Indonesians']
+handler.command = /^kapan(kah)?$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
@@ -26,5 +26,4 @@ module.exports = handler
 
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
-}
-
+} 
