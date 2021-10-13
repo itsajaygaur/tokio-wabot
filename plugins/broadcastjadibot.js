6d3abd3b9e5323e3e5354f5f64ffbfd871803e38
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-13 23:26:44 +0530

    Update broadcastjadibot.js

--- plugins/broadcastjadibot.js
+++ plugins/broadcastjadibot.js
@@ -3,14 +3,14 @@ let handler = async (m, { conn, usedPrefix, text }) => {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user.jid)])]
   let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
   let teks = text ? text : cc.text
-  let content = conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 All Jadibot Broadcast 」')
+  let content = conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 All Getbot Broadcast 」')
   for (let id of users) conn.copyNForward(id, content, true)
   conn.reply(m.chat, `_Send a broadcast message to ${users.length} number that became bot_
 ${users.map(v => 'wa.me/' + v.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu`).join('\n')}`.trim(), m)
 }
-handler.help = ['broadcastjadibot', 'bcbot'].map(v => v + ' <teks>')
+handler.help = ['broadcastgetbot/bcbot'].map(v => v + ' <teks>')
 handler.tags = ['host']
-handler.command = /^(broadcast|bc)(jadi)?bot$/i
+handler.command = /^(broadcast|bc)(get)?bot$/i
 handler.owner = true
 handler.mods = false
 handler.premium = false
