87e8c0aea03614335d5a44c78d996848b7e2c053
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-13 21:26:55 +0530

    Update textpro.js

--- plugins/textpro.js
+++ plugins/textpro.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args: [effect], text: txt }) => {
   let { effects } = await (await (fetch(global.API('xteam', '/textpro')))).json()
   if (!effect) throw '*List Effect*\n\n' + effects.sort((a, b) => a - b).join('\n')
   effect = effect.toLowerCase()
-  if (!effect in effects) throw `Efek *${effect}* tidak ditemukan`
+  if (!effect in effects) throw `Effect *${effect}* not found`
   let [text, ...text2] = txt.replace(effect, '').trimStart().split(split)
   text2 = text2.join(split)
   let url = global.API('xteam', '/textpro/' + effect, { text, text2 }, 'APIKEY')
