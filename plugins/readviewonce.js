00907893e8afcf002d77175a730f84d848841bc3
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-13 21:07:40 +0530

    Update readviewonce.js

--- plugins/readviewonce.js
+++ plugins/readviewonce.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text }) => {
     if (!m.quoted) return conn.sendMessage(m.chat, 'where\'s message?', 'conversation')
-    if (m.quoted.mtype !== 'viewOnceMessage') throw 'Itu bukan pesan viewOnce'
-    await conn.copyNForward(m.chat, await conn.loadMessage(m.chat, m.quoted.id), false, { readViewOnce: true }).catch(_ => m.reply('Mungkin dah pernah dibuka bot'))
+    if (m.quoted.mtype !== 'viewOnceMessage') throw 'That\'s not a viewonce message'
+    await conn.copyNForward(m.chat, await conn.loadMessage(m.chat, m.quoted.id), false, { readViewOnce: true }).catch(_ => m.reply('Maybe it\'s been opened by a bot'))
 }
 
 handler.help = ['readviewonce']
