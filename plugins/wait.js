272772d612383258d95db7fabd7c3b7289cc85c6
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-13 21:33:35 +0530

    Update wait.js

--- plugins/wait.js
+++ plugins/wait.js
@@ -5,8 +5,8 @@ const fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw `Reply Foto/Kirim Foto Dengan Caption ${usedPrefix}wait`
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
+  if (!mime) throw `Reply Photo/Send Photo With Caption ${usedPrefix}wait`
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Meme ${mime} not supported`
   let img = await q.download()
   await m.reply('Searching Anime Titles...')
   let anime = `data:${mime};base64,${img.toString('base64')}`
@@ -17,15 +17,15 @@ let handler = async (m, { conn, usedPrefix }) => {
     },
     body: JSON.stringify({ image: anime }),
   })
-  if (!response.ok) throw 'Gambar tidak ditemukan!'
+  if (!response.ok) throw 'Image not found!'
   let result = await response.json()
   let { is_adult, title, title_chinese, title_romaji, episode, season, similarity, filename, at, tokenthumb, anilist_id } = result.docs[0]
   let link = `https://media.trace.moe/video/${anilist_id}/${encodeURIComponent(filename)}?t=${at}&token=${tokenthumb}`
   let nobuyaki = `
-${similarity < 0.89 ? 'Saya Memiliki Keyakinan Rendah Tentang Hal Ini' : ''}
+${similarity < 0.89 ? 'I Have Low Confidence About This' : ''}
 
-❏ Judul Jepang : *${title}*
-❏ Ejaan Judul : *${title_romaji}*
+❏ Japanese title : *${title}*
+❏ Spelling Title : *${title_romaji}*
 ❏ Similarity : *${(similarity * 100).toFixed(1)}%*
 ❏ Episode : *${episode.toString()}*
 ❏ Ecchi : *${is_adult ? 'Yes' : 'No'}*
