c3fe6f5277374f50b8a50673d0d4ce0ee0e4bb2b
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-14 12:53:25 +0530

    Update qrcode.js

--- plugins/qrcode.js
+++ plugins/qrcode.js
@@ -3,9 +3,9 @@ let qrcode = require("qrcode")
 let handler  = async (m, { conn, text }) => {
   conn.sendFile(m.chat, await qrcode.toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
 }
-handler.help = ['qr/qrcode <teks>')
+handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
 handler.tags = ['tools']
-handler.command = /^(qr|qrcode)$/i
+handler.command = /^qr(code)?$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
@@ -18,4 +18,3 @@ handler.botAdmin = false
 handler.fail = null
 
 module.exports = handler
-
