e8206d2cd444daf1c332d59c42a2fab6b635715a
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-14 12:28:12 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -117,7 +117,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
       ...Object.keys(tags).map(tag => {
         return header.replace(/%category/g, tags[tag]) + '\n' + [
           ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
-            return (help => {
+            return  menu.help.map(help => {
               return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
                 .replace(/%islimit/g, menu.limit ? '(Limit)' : '')
                 .replace(/%isPremium/g, menu.premium ? '(Premium)' : '')
