827f90c24c689371c4fdd55a41847f4f4396373d
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-14 22:09:42 +0530

    Update _tictactoe.js

--- plugins/_tictactoe.js
+++ plugins/_tictactoe.js
@@ -10,7 +10,7 @@ handler.before = function (m) {
     let room = Object.values(this.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender) && room.state == 'PLAYING')
     if (room) {
         // m.reply(`[DEBUG]\n${parseInt(m.text)}`)
-        if (!/^([1-9]|(me)?giveup|surr?ender)$/i.test(m.text)) return !0
+        if (!/^([1-9]|(me)?quit|surr?ender)$/i.test(m.text)) return !0
         isSurrender = !/^[1-9]$/.test(m.text)
         if (m.sender !== room.game.currentTurn) { // nek wayahku
             if (!isSurrender) return !0
@@ -55,8 +55,8 @@ ${arr.slice(0, 3).join('')}
 ${arr.slice(3, 6).join('')}
 ${arr.slice(6).join('')}
 
-${isWin ? `@${(isSurrender ? room.game.currentTurn : room.game.winner).split('@')[0]} Win!` : isTie ? 'Game over' : `Waiting @${room.game.currentTurn.split('@')[0]}`}
-Type *giveup* to give up
+${isWin ? `@${(isSurrender ? room.game.currentTurn : room.game.winner).split('@')[0]} Win! 🏆` : isTie ? 'Game over' : `Waiting @${room.game.currentTurn.split('@')[0]}`}
+Type *quit* to give up
 `.trim()
         if ((room.game._currentTurn ^ isSurrender ? room.x : room.o) !== m.chat)
             room[room.game._currentTurn ^ isSurrender ? 'x' : 'o'] = m.chat
