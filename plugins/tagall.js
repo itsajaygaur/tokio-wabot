8f386b8f3422f478e0fb380355d97bdd102d8103
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-16 10:39:11 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => u.jid)
-    m.reply(`${text ? `${text}\n` : ''}┌─〔 Tag All 〕\n` + users.map(v => '├ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, {
-    contextInfo: { mentionedJid: users } 
+  m.reply(text + '\n\n' + users.map(v => '🎗️@' + v.replace(/@.+/, '')).join`\n`, null, {
+    contextInfo: { mentionedJid: users }
   })
 }
 handler.command = ['tagall']
