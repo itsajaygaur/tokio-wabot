9faff7cee5fa8817a8485f451c3158bc61345fe0
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-16 23:18:16 +0530

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,8 +1,8 @@
-const { igdl } = require('../lib/scrape')
+let fetch = require('node-fetch')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-  if (!args[0]) throw `uhm.. where is URL?\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/CQU21b0JKwq/`
+  if (!args[0]) throw `uhm.. where is URL?\n\nexample:\n${usedPrefix + command} https://www.instagram.com/p/CQU21b0JKwq/`
   if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `wrong url, this command to download post/reel/tv`
 
   igdl(args[0]).then(async res => {
