8c50a4969ed9ae4b2eac90477f4b6ee96b2b8e83
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-17 21:16:04 +0530

    Update textpro.js

--- plugins/textpro.js
+++ plugins/textpro.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let split = '|'
 let handler = async (m, { conn, args: [effect], text: txt }) => {
   let { effects } = await (await (fetch(global.API('xteam', '/textpro')))).json()
-  if (!effect) throw '*List Effect*\n\n' + effects.sort((a, b) => a - b).join('\n')
+  if (!effect) throw '*List of Effects*\n\n' + effects.sort((a, b) => a - b).join('\n')
   effect = effect.toLowerCase()
   if (!effect in effects) throw `Effect *${effect}* not found`
   let [text, ...text2] = txt.replace(effect, '').trimStart().split(split)
@@ -10,9 +10,9 @@ let handler = async (m, { conn, args: [effect], text: txt }) => {
   let url = global.API('xteam', '/textpro/' + effect, { text, text2 }, 'APIKEY')
   await conn.sendFile(m.chat, url, 'textpro.jpg', `*TEXTPRO*\n*Effect:* ${effect}`, m)
 }
-handler.help = ['textpro'].map(v => v + ' <effect> <text>|[text2]')
+handler.help = ['textpro/tp'].map(v => v + ' <effect> <text>|<text2>')
 handler.tags = ['tools']
-handler.command = /^(textpro)$/i
+handler.command = /^(textpro|tp)$/i
 
 module.exports = handler
 
