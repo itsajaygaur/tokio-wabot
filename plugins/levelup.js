47bb2c8ddf51f56e03741dbaef4543006b0bc360
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-17 20:55:07 +0530

    Update levelup.js

--- plugins/levelup.js
+++ plugins/levelup.js
@@ -6,16 +6,16 @@ let handler = m => {
     let { min, xp, max } = levelling.xpRange(user.level, global.multiplier)
     throw `
 Level *${user.level} (${user.exp - min}/${xp})*
-Kurang *${max - user.exp}* lagi!
+Not enough *${max - user.exp}* again!
 `.trim()
   }
   let before = user.level * 1
 	while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
 	if (before !== user.level) {
             m.reply(`
-Selamat, anda telah naik level!
+🎊 Congratulations, you have leveled up!
 *${before}* -> *${user.level}*
-gunakan *.profile* untuk mengecek
+use *.profile* to check
 	`.trim())
         }
 }
