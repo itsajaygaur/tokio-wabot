596c521f610e7282f556626d2e3029b02305b8dd
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-17 21:18:15 +0530

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,28 +1,24 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Uhm... where is url?'
-  let res = await fetch(global.API('xteam', '/dl/ig', {
-    url: args[0]
-  }, 'APIKEY'))
-  if (res.status !== 200) {
-    res.text()
-    throw res.status
-  }
-  let json = await res.json()
-  if (!json.result) throw json
-  let { name, username, likes, caption, data } = json.result
-  let text = `
-Username: ${name} *(@${username})*
-${likes} Likes
-Caption:
-${caption}
-`.trim()
-  for (let { data: url, type } of data)
-    conn.sendFile(m.chat, url, 'ig' + (type == 'video' ? '.mp4' : '.jpg'), text, m)
+const { igdl } = require('../lib/scrape')
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+
+  if (!args[0]) throw `uhm.. where is the url?\n\nExample:\n${usedPrefix + command} https://www.instagram.com/`
+  if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `wrong url, this command to download post/reel/tv`
+
+  igdl(args[0]).then(async res => {
+    let igdl = JSON.stringify(res)
+    let json = JSON.parse(igdl)
+    await m.reply(global.wait)
+    for (let { downloadUrl, type } of json) {
+      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), watermark, m)
+    }
+  })
+
 }
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
+handler.command = /^(ig|instagram)$/i
 
-handler.command = /^(ig(dl)?)$/i
+handler.limit = true
 
 module.exports = handler
