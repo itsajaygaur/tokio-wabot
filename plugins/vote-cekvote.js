a33a34754872e5eebbe2f76eb49423d851c1eafc
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-17 21:03:51 +0530

    Update vote-cekvote.js

--- plugins/vote-cekvote.js
+++ plugins/vote-cekvote.js
@@ -18,7 +18,7 @@ ${upvote.map(u => '@' + u.split('@')[0]).join('\n')}
 _Total: ${devote.length}_
 ${devote.map(u => '@' + u.split('@')[0]).join('\n')}
 
-*${usedPrefix}unvote* - to delete votes
+*${usedPrefix}delvote* - to delete votes
 
 _by ariffb_
 `.trim(), false, { contextInfo: { mentionedJid } })
