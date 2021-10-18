ec560be64c08b7ac80e34f693e6f20849524201c
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-18 23:49:22 +0530

    Update brainly.js

--- plugins/brainly.js
+++ plugins/brainly.js
@@ -1,11 +1,15 @@
-const brainly = require('brainly-scraper-v2')
-let handler = async function (m, { text }) {
-  if (!text) throw 'the problem?'
-  let res = await brainly(text)
-  let answer = res.data.map((v, i) => `_*QUESTION TO ${i + 1}*_\n${v.question}\n${v.answer.map((v,i) => `*ANSWER TO ${i + 1}*\n${v.text}`).join('\n')}`).join('\n\n•------------•\n\n')
-  m.reply(answer)
+const Brainly = require('brainly-scraper-v2')
+const brain = new Brainly("en")
+let handler = async function (m, { text, usedPrefix, command }) {
+  if (!text) throw `uhm.. where's the question?\n\nexample:\n${usedPrefix + command} what is javascript?``
+  brain.search("id", text).then(async res => {
+    let br = JSON.stringify(res)
+    let json = JSON.parse(br)
+    let answer = json.map((v, i) => `_*QUESTION TO ${i + 1}*_\n${v.question.content}\n${v.answers.map((v, i) => `*ANSWER TO ${i + 1}*\n${v.content.replace(/<\/?p>|<\/?strong>|<\/?u>|<\/?h[1-3]>|<\/?span>/g, '').replace(/<br ?(\/|\\)?>/g, '\n')}`).join('\n')}`).join('\n\n•------------•\n\n')
+    m.reply(answer)
+  })
 }
-handler.help = ['brainly <about>']
+handler.help = ['brainly <question>']
 handler.tags = ['internet']
 
 handler.command = /^brainly$/i
