3b0f7af678c12f0f409baa13690923931c6856a1
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-18 11:52:53 +0530

    Update animeinfo.js

--- plugins/animeinfo.js
+++ plugins/animeinfo.js
@@ -23,7 +23,7 @@ let animeingfo = `✨️ *Title:* ${title}
   conn.sendFile(m.chat, image_url, '', animeingfo, m)
 }
 handler.help = ['anime <title>']
-handler.tags = ['internet']
+handler.tags = ['anime']
 handler.command = /^(anime|animeinfo)$/i
 //maapin fatur :<
 module.exports = handler
