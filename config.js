6cb277970825da3ab5b23b6de8d947251322371b
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-19 21:07:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.xteam.xyz': 'MahliKey',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'x17SNoSIMmJY6sG4hhYF2k5TRxW',
   'https://pencarikode.xyz': 'pais',
