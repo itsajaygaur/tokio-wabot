d51bfdfc959d17826ed9995a237acd5870435921
thereal666 <79410810+thereal666@users.noreply.github.com>
2021-10-23 18:44:58 +0530

    Update brainly.js

--- plugins/brainly.js
+++ plugins/brainly.js
@@ -1,6 +1,6 @@
 const brainly = require('brainly-scraper-v2')
 let handler = async function (m, { text }) {
-  if (!text) throw 'what's the question?'
+  if (!text) throw 'what\'s the question?'
   let res = await brainly(text)
   let answer = res.data.map((v, i) => `_*QUESTION TO ${i + 1}*_\n${v.pertanyaan}\n${v.jawaban.map((v,i) => `*ANSWER TO ${i + 1}*\n${v.text}`).join('\n')}`).join('\n\n•------------•\n\n')
   m.reply(answer)
