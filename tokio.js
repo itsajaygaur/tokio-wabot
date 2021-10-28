c5c97d008396381b2d2afbebf9344a54d778af16
Ajay Gaur <79410810+itsajaygaur@users.noreply.github.com>
2021-10-28 14:23:20 +0530

    Delete tokio.js

--- tokio.js
+++
@@ -1,8 +0,0 @@
-let handler = async m => m.reply(`
-https://github.com/itsajaygaur/tokio-wabot
-`.trim()) // repository
-handler.help = ['tokio']
-handler.tags = ['info']
-handler.command = /^tokio|repo$/i
-
-module.exports = handler
