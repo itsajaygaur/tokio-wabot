5de3933d48e1dbd1051f05f3cb6f546d771d6d75
Ajay Gaur <79410810+itsajaygaur@users.noreply.github.com>
2021-10-28 15:14:49 +0530

    Update tokio.js

--- plugins/tokio.js
+++ plugins/tokio.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
-https://github.com/itsajaygaur/tokio-wabot
+☕ *Tokio :* a whatsapp bot\n\n🔗 *URL:* https://github.com/itsajaygaur/tokio-wabot
 `.trim()) // repository
-handler.help = ['tokio']
+handler.help = ['tokio/repo']
 handler.tags = ['info']
 handler.command = /^tokio|repo$/i
 
