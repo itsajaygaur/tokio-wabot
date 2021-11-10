9798947b58d7db0ac8cf6ed684bda5a47668763b
Ajay Gaur <79410810+itsajaygaur@users.noreply.github.com>
2021-11-10 10:19:57 +0530

    Update hug.js

--- plugins/hug.js
+++ plugins/hug.js
@@ -1,35 +1,28 @@
-let fetch = require("node-fetch");
-let handler = async (m, { conn, command }) => {
-	if (m.quoted && m.quoted.sender) m.mentionedJid.push(m.quoted.sender);
-	if (!m.mentionedJid.length) m.mentionedJid.push(m.sender);
-	let res = await fetch(
-		API("https://some-random-api.ml", "/animu/" + command, {})
-	);
-	if (!res.ok) throw `${res.status} ${res.statusText}`;
-	let json = await res.json();
-	if (json.link)
-		conn.sendFile(
-			m.chat,
-			json.link,
-			"wibukontol.gif",
-			`@${m.sender.split("@")[0]} ${command} ${m.mentionedJid
-				.map((user) =>
-					user === m.sender ? "themselves " : `@${user.split("@")[0]}`
-				)
-				.join(", ")}`,
-			m,
-			false,
-			{
-				asGIF: true,
-				contextInfo: {
-					mentionedJid: [...m.mentionedJid, m.sender],
-				},
-			}
-		);
-	else throw json;
-};
-handler.help = ["hug", "pat", "wink"];
-handler.tags = ["fun"];
-handler.command = /^(hug|pat|wink)$/i;
+let fs = require("fs")
+let fetch = require("node-fetch")
+let { exec } = require("child_process")
 
-module.exports = handler;
+let handler = async (m, { conn, command }) => {
+	if (m.quoted && m.quoted.sender) m.mentionedJid.push(m.quoted.sender)
+	if (!m.mentionedJid.length) m.mentionedJid.push(m.sender)
+	let res = await fetch(API("https://api.waifu.pics", "/sfw/" + command, {}))
+	if (!res.ok) throw `${res.status} ${res.statusText}`
+	let json = await res.json()
+	if (json.url) {
+		let buf = await (await fetch(json.url)).buffer()
+		let input = `${command}.gif`
+		let output = `${command}.mp4`
+		fs.writeFileSync(input, buf)
+		exec(`ffmpeg -i ${input} -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" ${output}`, (e) => {
+			if (e) m.reply(String(e))
+			fs.unlinkSync(input)
+			conn.sendMessage(m.chat, fs.readFileSync(output), "videoMessage", { mimetype: "video/gif", quoted: m, caption: `@${m.sender.split("@")[0]} ${command} ${m.mentionedJid.map((user) => user === m.sender ? "themselves " : `@${user.split("@")[0]}`).join(", ")}`, contextInfo: { mentionedJid: [...m.mentionedJid, m.sender] }})
+			fs.unlinkSync(output)
+		})
+	} else throw json
+}
+handler.help = ["hug", "pat", "punch", "lick", "slap", "bully", "kiss"]
+handler.tags = ["fun"]
+handler.command = /^(hug|pat|punch|lick|slap|bully|kiss)$/i
+
+module.exports = handler
