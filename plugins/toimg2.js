9d45ee4cf7065532b036c03093b4e1c7e5a86dc6
Ajay Gaur <79410810+itsajaygaur@users.noreply.github.com>
2021-11-15 11:39:33 +0530

    Update toimg2.js

--- plugins/toimg2.js
+++ plugins/toimg2.js
@@ -3,7 +3,7 @@ let { webp2png } = require('../lib/webp2mp4')
 let handler = async (m, { conn, usedPrefix, command }) => {
   if (!m.quoted) throw `reply sticker with caption *${usedPrefix + command}*`
   let mime = m.quoted.mimetype || ''
-  if (!/webp/.test(mime)) throw `balas stiker dengan caption *${usedPrefix + command}*`
+  if (!/webp/.test(mime)) throw `reply sticker with caption *${usedPrefix + command}*`
   let media = await m.quoted.download()
   let out = Buffer.alloc(0)
   if (/webp/.test(mime)) {
