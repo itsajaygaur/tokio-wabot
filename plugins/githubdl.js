b864db0630681b6ac86b2dfe2cb3a8ae75cd1c44
Ajay Gaur <79410810+itsajaygaur@users.noreply.github.com>
2021-12-01 15:36:21 +0530

    Update githubdl.js

--- plugins/githubdl.js
+++ plugins/githubdl.js
@@ -5,12 +5,12 @@ if (!args[1]) throw 'where\'s the repo?'
 
 let url = `https://github.com/${args[0]}/${args[1]}/zipball/master`
 //ByRizkyAdi
-m.reply(`compressing data to file zip*`)
+m.reply(`compressing data to zip file`)
 conn.sendFile( m.chat, url, 'repo.zip', null, m)
 
 }
-handler.help = ['githubdl']
-handler.tags = ['download']
+handler.help = ['githubdl <username> <repo>']
+handler.tags = ['downloader']
 handler.command = /githubdl/i
 
 handler.limit = true
