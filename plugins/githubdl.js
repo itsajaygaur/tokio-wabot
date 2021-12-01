a1b45ab401a8d5001b9a5ace194e33e4a24a3e78
Ajay Gaur <79410810+itsajaygaur@users.noreply.github.com>
2021-12-01 15:27:26 +0530

    Update githubdl.js

--- plugins/githubdl.js
+++ plugins/githubdl.js
@@ -3,7 +3,7 @@ let handler = async (m, { args, usedPrefix, command }) => {
 if (!args[0]) throw 'where\'s the username ?'
 if (!args[1]) throw 'where\'s the repo?'
 
-let url = `https://github.com/${args[0]}/${args[1]}/archive/refs/heads/main.zip`
+let url = `https://github.com/${args[0]}/${args[1]}/zipball/master`
 //ByRizkyAdi
 m.reply(`compressing data to file zip*`)
 conn.sendFile( m.chat, url, 'repo.zip', null, m)
