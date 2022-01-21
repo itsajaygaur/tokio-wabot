d40bd08cdbce1a64680ab7805d44fe3c307ea7ef
Ajay Gaur <79410810+itsajaygaur@users.noreply.github.com>
2022-01-21 10:08:37 +0530

    Update stickerTelegram.js
    stickertelegram fixed ⚒️

--- plugins/stickerTelegram.js
+++ plugins/stickerTelegram.js
@@ -1,21 +1,24 @@
 const fetch = require('node-fetch')
-const { MessageType } = require('@adiwajshing/baileys')
-const { sticker } = require('../lib/sticker')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `*This command to retrieve stickers from Telegram*\n\nexample:\n${usedPrefix + command} https://t.me/addstickers/menggokil`
-    if (!args[0].match(/(https:\/\/t.me\/addstickers\/)/gi)) throw `*wrong link! This command to retrieve stickers from Telegram*\n\nexample:\n${usedPrefix + command} https://t.me/addstickers/menggokil`
+const { sticker4 } = require('../lib/sticker')
 
-    let res = await fetch(global.API('zeks', '/api/telegram-sticker', { url: args[0] }, 'apikey'))
-    if (res.status != 200) throw await `${res.status} ${res.statusText}`
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Use:\n${usedPrefix + command} <url>\n\nExample:\n${usedPrefix + command} https://t.me/addstickers/namapack`
+    if (!args[0].match(/(https:\/\/t.me\/addstickers\/)/gi)) throw `Invalid url`
+    let packName = args[0].replace('https://t.me/addstickers/', '')
+    let res = await fetch(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getStickerSet?name=${encodeURIComponent(packName)}`, { method: 'GET', headers: { 'User-Agent': 'GoogleBot' } })
+    if (!res.ok) throw error
     let json = await res.json()
-    if (!json.status) throw json
-    let mes = json.result.map((v, i) => `${i + 1}. ${v}`).join('\n')
-    m.reply(`*Total:* ${json.result.length}\n*Estimated complete:* ${json.result.length * 2} seconds\n\n` + mes)
-    for (let i of json.result) {
-        stiker = await sticker(false, i, global.packname, global.author)
-        await conn.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m })
-        await delay(2000)
+    m.reply(`*Total sticker:* ${json.result.stickers.length}
+*Estimated time for completion:* ${json.result.stickers.length * 1.5} seconds`.trim())
+    for (let i = 0; i < json.result.stickers.length; i++) {
+        let fileId = json.result.stickers[i].thumb.file_id
+        let res = await fetch(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getFile?file_id=${fileId}`)
+        let jisin = await res.json()
+        let stiker = await sticker4(false, "https://api.telegram.org/file/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/" + jisin.result.file_path, packname, author)
+        await conn.sendFile(m.chat, stiker, '', '', 0, 0, { asSticker: true })
+        //await conn.delay(1500)
     }
+    m.reply('_*Finished*_')
 }
 handler.help = ['stickertelegram <url>']
 handler.tags = ['sticker']
@@ -24,5 +27,3 @@ handler.command = /^(stic?kertele(gram)?)$/i
 handler.limit = true
 
 module.exports = handler
-
-const delay = time => new Promise(res => setTimeout(res, time))
