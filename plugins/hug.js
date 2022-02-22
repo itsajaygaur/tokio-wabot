735785b7ff6b5bab8ba713a7ea612a5cd2164192
Ajay Gaur <79410810+itsajaygaur@users.noreply.github.com>
2022-02-22 15:42:42 +0530

    Update hug.js

--- plugins/hug.js
+++ plugins/hug.js
@@ -16,13 +16,11 @@ let handler = async (m, { conn, command }) => {
 		exec(`ffmpeg -i ${input} -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" ${output}`, (e) => {
 			if (e) m.reply(String(e))
 			fs.unlinkSync(input)
-			conn.sendMessage(m.chat, fs.readFileSync(output), "videoMessage", { mimetype: "video/gif", quoted: m, caption: `@${m.sender.split("@")[0]} ${command} ${m.mentionedJid.map((user) => user === m.sender ? "themselves " : `@${user.split("@")[0]}`).join(", ")}`, contextInfo: { mentionedJid: [...m.mentionedJid, m.sender] }})
+			conn.sendMessage(m.chat, fs.readFileSync(output), "videoMessage", { mimetype: "video/gif", quoted: m, caption: `@${m.sender.split("@")[0]} *${command}* ${m.mentionedJid.map((user) => user === m.sender ? "*themselves* " : `@${user.split("@")[0]}`).join(", ")}`, contextInfo: { mentionedJid: [...m.mentionedJid, m.sender] }})
 			fs.unlinkSync(output)
 		})
 	} else throw json
 }
-handler.help = ["hug", "pat", "bonk", "lick", "slap", "bully", "kiss"]
-handler.tags = ["fun"]
-handler.command = /^(hug|pat|punch|lick|slap|bully|kiss)$/i
+handler.command = /^(hug|pat|lick|slap|bully|kiss|cry|cuddle|smug|highfive|bonk|yeet|blush|wave|smile|handhold|nom|bite|glomp|kill|cringe|kick|wink|happy|poke|dance)$/i
 
 module.exports = handler
