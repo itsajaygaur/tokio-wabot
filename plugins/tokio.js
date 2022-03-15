677337860280506e7c2118a741b6c4a52afd0c2d
Titan <101502610+Itstitan@users.noreply.github.com>
2022-03-16 03:13:12 +0530

    Update tokio.js

--- plugins/tokio.js
+++ plugins/tokio.js
@@ -1,8 +1,8 @@
 let handler = async m => m.reply(`
-☕ *Tokio :* a whatsapp bot\n\n🔗 *URL :* https://github.com/itsajaygaur/tokio-wabot
+☕ *Miko </3 :* a whatsapp bot\n\n🔗 *URL :* https://github.com/Itstitan/miku /n [open for public]
 `.trim()) // repository
 handler.help = ['tokio']
 handler.tags = ['info']
-handler.command = /^tokio|repo$/i
+handler.command = /^miko|repo$/i
 
 module.exports = handler
