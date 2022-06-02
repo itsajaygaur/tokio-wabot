02f3175b6fa1357f3ea838049f88267d20344fbc
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2022-06-03 00:13:00 +0300

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // This is just an example, do not uncomment -_-
   // F this.sendContact(m.chat, '254114148625', 'kanambo', '2348106542427' , 'owner love' m)
-  this.sendContact(m.chat, '254114148625' , 'kanambo' , '+2348106542427' , 'owner love', m)
+  this.sendContact(m.chat, '254114148625' , 'kanambo' , '2348106542427' , 'owner love', m)
 }
 handler.help = ['owner/creator']
 handler.tags = ['info']
