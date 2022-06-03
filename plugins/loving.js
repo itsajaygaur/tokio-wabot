221ba3729c18a80370d039eec8ff6da32fe59712
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2022-06-03 08:28:54 +0300

    Create loving.js

---
+++ plugins/loving.js
@@ -0,0 +1,13 @@
+let handler = async (m, { conn }) => {
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/loving', {
+    avatar: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+  }), 'loving.png', '❤️pure loving', m)
+}
+
+handler.help = ['loving']
+handler.tags = ['maker']
+
+handler.command = /^(loving)$/i
+
+module.exports = handler
