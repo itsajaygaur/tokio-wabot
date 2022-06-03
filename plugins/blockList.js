216f29edf138d03aee6c0ac90c487fa7ec1268df
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2022-06-03 08:48:46 +0300

    Update blockList.js

--- plugins/blockList.js
+++ plugins/blockList.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
     let blocked = conn.blocklist.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
-    conn.reply(m.chat, `┌ *「 Blocked List 」*` + `\n` + blocked.map((v, i) => `│ ${i + 1}. @${v.split`@`[0]}`).join`\n` + `\n└────`, m, { contextInfo: { mentionedJid: blocked } })
+    conn.reply(m.chat, `┌ *「👹 Blocked List 👹」*` + `\n` + blocked.map((v, i) => `│ ${i + 1}. @${v.split`@`[0]}`).join`\n` + `\n└────`, m, { contextInfo: { mentionedJid: blocked } })
 }
 handler.help = ['blocklist']
 handler.tags = ['owner']
